65ce59432d9ec3038a5674513d09755d70aecbaf
voidfunctionreturn0 <voidfunctionreturn0@gmail.com>
2024-08-18 22:52:41 +0900

    MarkerClustering 생성자 함수 로드 방법을 elixir forum에 질문 남김

--- service/assets/js/app.js
+++ service/assets/js/app.js
@@ -22,6 +22,7 @@ import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 import MarkerClustering from "../vendor/marker_clustering"
+window.MarkerClustering = MarkerClustering
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {
